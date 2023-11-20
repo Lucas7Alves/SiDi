c2e659ab58e198d349e5cd3b0d56aebf2847ba47
Maria <ssalgado.mariajulia@gmail.com>
2023-11-20 15:46:58 -0300

    tela login nova bbs

--- BatePonto/src/pages/Home.tsx
+++ BatePonto/src/pages/Home.tsx
@@ -2,34 +2,30 @@ import Cabecalho from "../components/cabecalho";
 import { Button } from "@mui/material";
 import { useState } from "react";
 
-
 function Home() {
+  const [message, setMessage] = useState("");
 
-    const [message, setMessage] = useState("");
-
-    const mostrarMessage = () => {
-        setMessage("Ponto Batido!") 
-    }
-    return(
-        <div>
-            <Cabecalho/>
-          <p>Bem-Vinda de volta, <strong>Maria Julia!</strong> Sentimos sua falta.</p>  
-           <Button
-           type="reset"
-           color="secondary"
-           variant="outlined"
-           size="large"
-           onClick={mostrarMessage}
-           
-           >
-            Bater Ponto
-           </Button>
-           {<p>{message}</p>}
-     
-        </div>
-
-
-    )
+  const mostrarMessage = () => {
+    setMessage("Ponto Batido!");
+  };
+  return (
+    <div>
+      <Cabecalho />
+      <p>
+        Bem-Vinda de volta, <strong>Maria Julia!</strong> Sentimos sua falta.
+      </p>
+      <Button
+        type="reset"
+        color="secondary"
+        variant="outlined"
+        size="large"
+        onClick={mostrarMessage}
+      >
+        Bater Ponto
+      </Button>
+      {<p>{message}</p>}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
