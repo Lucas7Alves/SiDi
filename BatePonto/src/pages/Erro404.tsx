8e8309b59962ddc9f2bec6675ae6433dd0d50e53
Bruno <brunolucasofc@gmail.com>
2023-11-21 20:53:01 -0300

    TELA DE ERRO404 FEITA POR BRUNO LINDO

--- BatePonto/src/pages/Erro404.tsx
+++ BatePonto/src/pages/Erro404.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@mui/material"
+import "../scss/erro404.sass";
+    
 function Erro404() {
     return(
         <>
-        <h1>Error 404!</h1>
-            Voltar para o Home
+            <div className="corFundoErro">
+                <div className="fundoErro">
+                    <h1 className="nomeErro"><br></br><br></br><br></br><br></br><br></br>
+                    Ops, esta página<br></br>não foi encontrada.</h1>
+                    <p className="fraseErro">A página que você procura pode ter sido removida, ter seu<br></br>nome alterado ou estar temporariamente indisponível.</p>
+
+                </div>
+            </div>    
         </>
     )
 }
