e5ecb932ad6f49c165aa95a11dc0dd8576886958
Lucas Alves <lucassalvess0909@gmail.com>
2023-11-21 21:27:35 -0300

    btn volta do error
    + um sasszinho

--- BatePonto/src/pages/Erro404.tsx
+++ BatePonto/src/pages/Erro404.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@mui/material"
 import "../scss/erro404.sass";
+import { useNavigate } from "react-router-dom";
     
 function Erro404() {
+    const navigate = useNavigate();
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+          navigate("/login");
+      };
+
     return(
         <>
             <div className="corFundoErro">
@@ -9,7 +16,15 @@ function Erro404() {
                     <h1 className="nomeErro"><br></br><br></br><br></br><br></br><br></br>
                     Ops, esta página<br></br>não foi encontrada.</h1>
                     <p className="fraseErro">A página que você procura pode ter sido removida, ter seu<br></br>nome alterado ou estar temporariamente indisponível.</p>
-
+                    <form onSubmit={handleSubmit} className="btnVolta">
+                        <Button 
+                            type="submit"
+                            color="secondary"
+                            variant="contained"
+                            size="large"
+                        >Página Inicial    
+                        </Button>
+                    </form>
                 </div>
             </div>    
         </>
