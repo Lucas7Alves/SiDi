8c81b7d9d6646645171331e9e541d2a79fb0684c
Bruno <brunolucasofc@gmail.com>
2023-11-23 18:56:34 -0300

    mais avanços

--- BatePonto/src/pages/Home.tsx
+++ BatePonto/src/pages/Home.tsx
@@ -10,6 +10,7 @@ import NavBar from "../components/NavBar";
 import FundoInicial from "../img/fundologinTela.svg";
 
 
+
 function Home() {
 
   const navigate = useNavigate();
@@ -18,11 +19,11 @@ function Home() {
     navigate("/login");
   };
 
-
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyCQI2HtvKCfvPZ8GXGI05UQzLM4ykDrU-U"
   })
+  
   const [message, setMessage] = useState("");
   const [messageCorrigida, setMsgCorrigida] = useState("");
 
@@ -35,72 +36,87 @@ function Home() {
   };
 
   return (
+
+
+
+<div className="fundoTelaInicial">
+  <NavBar/>
   
-    <div className="fundoTelaInicial">
-      <NavBar/>
-        <div className="telaInicialContainer">
-      
-      <div>
-        <p className="bemVindaHomeFrase">
-        Bem-vinda de volta, Maria!
-        </p>
+<div className="telaInicialContainer">
+    
+<p className="wellcomeFrase">Bem-vinda de volta, Maria!</p>
 
-      <p className="entradaPonto">Entrada:</p>
-        <DateForm/>
-        <TimeBar/>
-          <Button
-            type="reset"
-            color="secondary"
-            variant="contained"
-            size="large"
-          onClick={mostrarMessage}
-          >Bater Ponto</Button>
-        {<p>{message}</p>}
+<div className="textosCima">
+  <p>Entrada:</p>
+  <p>Saída:</p>
+  <p>Corrigir ponto:</p>
+</div>
 
 
-        <p className="saidaPonto">Saída:</p>
-        <DateForm/>
-        <TimeBar/>
-          <Button
-            type="reset"
-            color="secondary"
-            variant="contained"
-            size="large"
-          onClick={mostrarMessage}
-          >Bater Ponto</Button>
-        {<p>{message}</p>}
+
+<div className="opcoesCima">
+
+<div className="form1">  
+<DateForm/>
+<TimeBar/>
+<Button
+type="reset"
+color="secondary"
+variant="contained"
+size="large"
+onClick={mostrarMessage}
+>Bater Ponto</Button>
+{<p>{message}</p>}
+</div>  
 
 
-      <p className="corrigirPonto">Corrigir ponto:</p>
-        <DateForm />  {}
-        <TimeBar />  {}
-          <Button 
-            type="reset"
-            color="secondary"
-            variant="contained"
-            size="large"
-            onClick={msgCorrigida}
-          >Corrigir ponto</Button>
-        {<p>{messageCorrigida}</p>}
+<div className="form2">  
+<DateForm/>
+<TimeBar/>
+<Button
+type="reset"
+color="secondary"
+variant="contained"
+size="large"
+onClick={mostrarMessage}
+>Bater Ponto</Button>
+{<p>{message}</p>}
+</div>
 
 
-      
+<div className="form3">  
+<DateForm />  {}
+<TimeBar />  {}
+<Button 
+type="reset"
+color="secondary"
+variant="contained"
+size="large"
+onClick={msgCorrigida}
+>Corrigir ponto</Button>
+{<p>{messageCorrigida}</p>}
+</div>
+
+</div>
+
+<p className="textoMapa">Sua localização:</p>
+
 <div className="map">{isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={{width: 500, height: 300}}
-        center={{
-          lat: -8.087133654385111, 
-          lng: -34.89179401238508
-        }}
-        zoom={15}
-      >
-        
-      </GoogleMap>
-  ) : <></>
-  }</div>
-    </div>
-    </div>
-    </div>
+  <GoogleMap
+    mapContainerStyle={{width: 500, height: 300}}
+    center={{
+    lat: -8.087133654385111, 
+    lng: -34.89179401238508
+    }}
+    zoom={15}>
+    </GoogleMap>
+    ) : <></>
+  }
+</div>
+
+
+</div>
+</div>
 
 
   );
