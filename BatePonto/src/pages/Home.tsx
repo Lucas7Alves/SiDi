b64c7e09d57fa57b27fac368f936983f4bd467ad
Lucas Alves <lucassalvess0909@gmail.com>
2023-12-03 11:14:22 -0300

    pequenos ajustes no cod

--- BatePonto/src/pages/Home.tsx
+++ BatePonto/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import DateForm from "../components/dateForm"
+
 import "../scss/home.sass";
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import TimeBar from "../components/TimeBar";
@@ -39,10 +40,13 @@ function Home() {
     e.preventDefault();
 
     try {
+      { /* falta passar o form data */}
       const response = await axios.post(
         "http://127.0.0.1:5000/sidi_ponto/v1/criar"
         
+        "http://127.0.0.1:5000/sidi_ponto/v1/criar" 
       );
+      
       if (response.status === 200) {
         alert("Registro efetuado!");
       } else {
@@ -67,72 +71,72 @@ function Home() {
       <NavBar />
 
       <div className="telaInicialContainer">
+        
 
-        <p className="wellcomeFrase">⠀⠀Bem-vindo de volta, Lucas!</p>
+          <p className="wellcomeFrase">⠀⠀Bem-vindo de volta, Lucas!</p>
 
-        <div className="textosCima">
-          <p className="entradaName">⠀⠀⠀⠀⠀⠀⠀⠀⠀Entrada:</p>
-          <p className="saidaName">⠀⠀⠀⠀⠀⠀Saída:⠀⠀⠀</p>
-          <p className="corrigirName">Corrigir ponto:</p>
-        </div>
-
-
-
-        <div className="opcoesCima">
-
-          <div className="form1">
-            <form onSubmit={handleEntrada}>
-            <DatePicker value={selectedDate} onChange={handleDateChange} />
-            </form>
-
-            <TimeBar />
-            <button className="btnHomeI"
-              type="reset"
-              onClick={mostrarMessage}
-            >Bater Ponto</button>
-
+          <div className="textosCima">
+            <p className="entradaName">⠀⠀⠀⠀⠀⠀⠀⠀⠀Entrada:</p>
+            <p className="saidaName">⠀⠀⠀⠀⠀⠀Saída:⠀⠀⠀</p>
+            <p className="corrigirName">Corrigir ponto:</p>
           </div>
 
 
-          <div className="form2">
-            <DateForm />
-            <TimeBar />
-            <button className="btnHomeII"
-              type="reset"
-              onClick={mostrarMessageSaida}
-            >Bater Ponto</button>
-            {<p>{message}</p>}
+
+          <div className="opcoesCima">
+
+            <div className="form1">
+              <form onSubmit={handleEntrada}>
+                <DatePicker value={selectedDate} onChange={handleDateChange} />
+              </form>
+
+              <TimeBar />
+              <button className="btnHomeI"
+                type="reset"
+                onClick={mostrarMessage}
+              >Bater Ponto</button>
+
+            </div>
+
+
+            <div className="form2">
+
+              <TimeBar />
+              <button className="btnHomeII"
+                type="reset"
+                onClick={mostrarMessageSaida}
+              >Bater Ponto</button>
+              {<p>{message}</p>}
+            </div>
+
+
+            <div className="form3">
+
+              <TimeBar />  { }
+              <button className="btnHomeIII"
+                type="reset"
+
+                onClick={msgCorrigida}
+              >Corrigir ponto</button>
+              {<p>{messageCorrigida}</p>}
+            </div>
+
           </div>
 
+          <p className="textoMapa">Sua localização:⠀⠀⠀⠀</p>
 
-          <div className="form3">
-            <DateForm />  { }
-            <TimeBar />  { }
-            <button className="btnHomeIII"
-              type="reset"
-
-              onClick={msgCorrigida}
-            >Corrigir ponto</button>
-            {<p>{messageCorrigida}</p>}
+          <div className="map">{isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={{ width: 800, height: 280 }}
+              center={{
+                lat: -8.087133654385111,
+                lng: -34.89179401238508
+              }}
+              zoom={15}>
+            </GoogleMap>
+          ) : <></>
+          }
           </div>
-
-        </div>
-
-        <p className="textoMapa">Sua localização:⠀⠀⠀⠀</p>
-
-        <div className="map">{isLoaded ? (
-          <GoogleMap
-            mapContainerStyle={{ width: 800, height: 280 }}
-            center={{
-              lat: -8.087133654385111,
-              lng: -34.89179401238508
-            }}
-            zoom={15}>
-          </GoogleMap>
-        ) : <></>
-        }
-        </div>
-
 
       </div>
     </div>
